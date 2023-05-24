ab605c1aee33407c943aaa8783b053c2e0dc9cf5
ankitBorgohain <134446243+ankitBorgohain@users.noreply.github.com>
2023-05-24 19:30:36 +0530

    Add files via upload
    A simple Responsive Dashboard design using HTML, CSS, JavaScript.
    
    Amongst the Key Features are :
    -Day and Night mode functional toggle button
    -Simple Transition/Animations
    -User friendly design

---
+++ Responsive Admin Dashboard using HTML, CSS, JS/orders.js
@@ -0,0 +1,38 @@
+const Orders= [
+    {
+    productName: 'Foldable Chair set',
+    productNumber:'124153',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName: 'Plain Long Sleeve Shirt',
+    productNumber:'981276',
+    paymentStatus:'Paid',
+    shipping:'Pending'
+},
+{
+    productName: 'Oneplus Nord 2CE',
+    productNumber:'156781',
+    paymentStatus:'Refunded',
+    shipping:'Declined'
+},
+{
+    productName: 'Samsung Speaker',
+    productNumber:'888333',
+    paymentStatus:'Paid',
+    shipping:'Shipped'
+},
+{
+    productName: 'Marshall',
+    productNumber:'811333',
+    paymentStatus:'Refunded',
+    shipping:'Declined'
+},
+{
+    productName: 'PS 5',
+    productNumber:'000333',
+    paymentStatus:'Paid',
+    shipping:'Pending'
+}
+]
